589cced47babfa24a8cdc0a84d486ec283b51afa
jspvg <josipovic1501@gmail.com>
2022-09-06 19:53:18 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const items = [
+    {
+        image: './images/cat.png',
+        answers: ['cat', 'kitty']
+    },
+    {
+        image: './images/dog.jpeg',
+        answers: ['dog']
+    },
+    {
+        image: './images/tree.png',
+        answers: ['tree', 'scape']
+    },
+    {
+        image: './images/laptop.webp',
+        answers: ['laptop', 'computer']
+    },
+    {
+        image: './images/bird.png',
+        answers: ['bird', 'birds']
+    }
+];
