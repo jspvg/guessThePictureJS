7140906ddb60e23b607e4e675646cd3051bdb846
jspvg <josipovic1501@gmail.com>
2022-09-06 19:57:27 +0200

    finished

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const result = document.getElementById('result');
+const image = document.getElementById('image');
+const answerInput = document.getElementById('answerInput');
+const nextBtn = document.getElementById('nextBtn');
+const restartBtn = document.getElementById('restartBtn');
+
 const items = [
     {
         image: './images/cat.png',
@@ -20,3 +26,43 @@ const items = [
         answers: ['bird', 'birds']
     }
 ];
+
+let change = 0;
+let points = 0;
+
+window.addEventListener('load', () => {
+    restartGame();
+});
+
+nextBtn.addEventListener('click', () => {
+    checkAnswer();
+});
+
+restartBtn.addEventListener('click', () => {
+    restartGame();
+});
+
+function checkAnswer(){
+
+    let answer = answerInput.value;
+
+    if(items[change].answers.includes(answer)){
+        points ++;
+    }
+
+    if(change < items.length-1){
+        change ++;
+        image.src = items[change].image;
+    } else{
+        result.classList.remove('hide');
+        result.innerText = `Broj osvojenih bodova je: ${points}.`;
+    }
+    
+}
+
+function restartGame(){
+    change = 0;
+    points = 0;
+    result.classList.add('hide');
+    image.src = items[0].image;
+}
